1e6c56066c292cbf1176943d9698bde6a20ac9e9
root <root@LAPTOP-655G2R71>
2025-05-15 16:58:48 +0700

    big update2

--- src/app/ui/dashboard/cards.tsx
+++ src/app/ui/dashboard/cards.tsx
@@ -1,3 +1,4 @@
+import { fetchCardData } from '@/app/lib/data';
 import {
   BanknotesIcon,
   ClockIcon,
@@ -6,6 +7,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { alegreya } from '@/app/ui/fonts';
 
+
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
@@ -13,19 +15,25 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
+
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices
+  } = await fetchCardData();
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      /> 
     </>
   );
 }
@@ -40,6 +48,7 @@ export function Card({
   type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
   const Icon = iconMap[type];
+
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
@@ -56,3 +65,6 @@ export function Card({
     </div>
   );
 }
+
+
+
