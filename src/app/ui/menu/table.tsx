a550de07ce89e260d51658e71c9d228195076ff8
root <root@LAPTOP-655G2R71>
2025-05-15 23:21:29 +0700

    add prisma

--- src/app/ui/menu/table.tsx
+++ src/app/ui/menu/table.tsx
@@ -1,22 +1,8 @@
-'use client';
+import { getProduk } from '@/app/lib/data';
 
-import React from 'react';
+export default async function ProdukTable() {
+  const produkList = await getProduk();
 
-interface Produk {
-  id_produk: number;
-  nama_produk: string;
-  harga: number;
-}
-
-const dummyProduk: Produk[] = [
-  { id_produk: 1, nama_produk: 'Espresso', harga: 20000 },
-  { id_produk: 2, nama_produk: 'Cappuccino', harga: 25000 },
-  { id_produk: 3, nama_produk: 'Latte', harga: 24000 },
-  { id_produk: 4, nama_produk: 'Americano', harga: 22000 },
-  { id_produk: 5, nama_produk: 'Mocha', harga: 26000 },
-];
-
-export default function ProdukTable() {
   return (
     <div className="overflow-x-auto rounded-md border border-gray-300">
       <table className="min-w-full divide-y divide-gray-200 text-sm">
@@ -28,7 +14,7 @@ export default function ProdukTable() {
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-100 bg-white text-gray-800">
-          {dummyProduk.map((produk) => (
+          {produkList.map((produk) => (
             <tr key={produk.id_produk}>
               <td className="px-4 py-2">{produk.id_produk}</td>
               <td className="px-4 py-2">{produk.nama_produk}</td>
