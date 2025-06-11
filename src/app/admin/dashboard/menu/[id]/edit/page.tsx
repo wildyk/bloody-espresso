397415908e8c2c45d851355d22cb67df6512829d
wildy kumaat <wildykumaat12@gmail.com>
2025-06-11 22:43:40 +0700

    update kecil12

--- src/app/admin/dashboard/menu/[id]/edit/page.tsx
+++ src/app/admin/dashboard/menu/[id]/edit/page.tsx
@@ -1,22 +1,29 @@
 export const dynamic = "force-dynamic";
 
-import { notFound } from 'next/navigation';
-import Form from '@/app/ui/menu/editform';
-import Breadcrumbs from '@/app/ui/menu/breadcrumbs';
-import { fetchProduk } from '@/app/lib/data';
-import { Produk } from '@/app/lib/definitions';
+import { notFound } from "next/navigation";
+import Form from "@/app/ui/menu/editform";
+import Breadcrumbs from "@/app/ui/menu/breadcrumbs";
+import { fetchProduk } from "@/app/lib/data";
+import { Produk } from "@/app/lib/definitions";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const produk = await fetchProduk(params.id);
-  if (!produk) notFound();
+interface Props {
+  params: { id: string };
+}
+
+export default async function Page({ params }: Props) {
+  const produk: Produk | null = await fetchProduk(params.id);
+
+  if (!produk) {
+    notFound();
+  }
 
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Menu', href: '/admin/dashboard/menu' },
+          { label: "Menu", href: "/admin/dashboard/menu" },
           {
-            label: 'Edit Menu',
+            label: "Edit Menu",
             href: `/admin/dashboard/menu/${params.id}/edit`,
             active: true,
           },
@@ -26,4 +33,3 @@ export default async function Page({ params }: { params: { id: string } }) {
     </main>
   );
 }
-
